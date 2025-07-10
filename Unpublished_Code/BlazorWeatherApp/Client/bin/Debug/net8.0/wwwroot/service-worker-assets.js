6c02986e833efdf53564ad15a27ec0c273502b33
Saket Khare <khare.saket@gmail.com>
2025-07-10 08:00:17 +0530

    Committed Weather Demo
    Committed Weather Demo incomplete code  v1.

---
+++ Unpublished_Code/BlazorWeatherApp/Client/bin/Debug/net8.0/wwwroot/service-worker-assets.js
@@ -0,0 +1,77 @@
+self.assetsManifest = {
+  "version": "oD8a8Qns",
+  "assets": [
+    {
+      "hash": "sha256-8YWwzs9F+HThvYEyqBeeAFPDkxHg8emiffTergFf69Q=",
+      "url": "_content/BlazorWeatherApp.Client/BlazorWeatherApp.Client.styles.css"
+    },
+    {
+      "hash": "sha256-E700bBhMgKiDDPvPdnhc7UNmgdNb2VMLq62T1u9n50w=",
+      "url": "_content/BlazorWeatherApp.Client/css/app.css"
+    },
+    {
+      "hash": "sha256-39I0HoYqzK/986SYAo8kurO0+66GzYYlkgtcWvgFT6c=",
+      "url": "_content/BlazorWeatherApp.Client/css/bootstrap/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-lPl+YkZ5F/Gqi+xd4+Juk6O2lf8947F0B9TDTF+fxSM=",
+      "url": "_content/BlazorWeatherApp.Client/css/bootstrap/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-BgJpRZSGsv2PCDyutlOHSV9vngLEp53cXBqPpTJlCvE=",
+      "url": "_content/BlazorWeatherApp.Client/css/open-iconic/FONT-LICENSE"
+    },
+    {
+      "hash": "sha256-lmR0kygUnUAknae38gH2rAsdPtwBPv4tfDUB8r3vMes=",
+      "url": "_content/BlazorWeatherApp.Client/css/open-iconic/ICON-LICENSE"
+    },
+    {
+      "hash": "sha256-X+5v7KP77EtMlvD1oXyYJliqxDhJ7LwWazrKCIGr/5c=",
+      "url": "_content/BlazorWeatherApp.Client/css/open-iconic/README.md"
+    },
+    {
+      "hash": "sha256-VKmQXW8mRZ7hoad6E/sXRxRWnDwfp6ihgfNDt3M7q9Q=",
+      "url": "_content/BlazorWeatherApp.Client/css/open-iconic/font/css/open-iconic-bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-OK3poGPgzKI2NzNgP07XMbJa3Dz6USoUh/chSkSvQpc=",
+      "url": "_content/BlazorWeatherApp.Client/css/open-iconic/font/fonts/open-iconic.eot"
+    },
+    {
+      "hash": "sha256-sDUtuZAEzWZyv/U1xl/9D3ehyU69JE+FvAcu5HQ+/a0=",
+      "url": "_content/BlazorWeatherApp.Client/css/open-iconic/font/fonts/open-iconic.otf"
+    },
+    {
+      "hash": "sha256-u+GkRX/3vmziPc6M/O+lTRu+QNOFBJg8DNTfcSFPI/k=",
+      "url": "_content/BlazorWeatherApp.Client/css/open-iconic/font/fonts/open-iconic.svg"
+    },
+    {
+      "hash": "sha256-p+RP8CV3vRK1YbIkNzq3rPo1jyETPnR07ULb+HVYL8w=",
+      "url": "_content/BlazorWeatherApp.Client/css/open-iconic/font/fonts/open-iconic.ttf"
+    },
+    {
+      "hash": "sha256-cZPqVlRJfSNW0KaQ4+UPOXZ/v/QzXlejRDwUNdZIofI=",
+      "url": "_content/BlazorWeatherApp.Client/css/open-iconic/font/fonts/open-iconic.woff"
+    },
+    {
+      "hash": "sha256-Jtxf9L+5ITKRc1gIRl4VbUpGkRNfOBXjYTdhJD4facM=",
+      "url": "_content/BlazorWeatherApp.Client/favicon.ico"
+    },
+    {
+      "hash": "sha256-DbpQaq68ZSb5IoPosBErM1QWBfsbTxpJqhU0REi6wP4=",
+      "url": "_content/BlazorWeatherApp.Client/icon-192.png"
+    },
+    {
+      "hash": "sha256-oEo6d+KqX5fjxTiZk/w9NB3Mi0+ycS5yLwCKwr4IkbA=",
+      "url": "_content/BlazorWeatherApp.Client/icon-512.png"
+    },
+    {
+      "hash": "sha256-cdDBHbfu9S+eWmwo23TsZqVVJN/dAz4/w+dqfjyc5xc=",
+      "url": "_content/BlazorWeatherApp.Client/index.html"
+    },
+    {
+      "hash": "sha256-U3EfdkWOpy8uKEKolDpzZobcZ7+QmFd5CXqm+ktA3h0=",
+      "url": "_content/BlazorWeatherApp.Client/manifest.json"
+    }
+  ]
+};
